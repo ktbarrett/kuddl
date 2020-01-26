1c3001a23cf7df84c3aecb3469ea4e3682044604
Kaleb Barrett <ktbarrett.connectflour@gmail.com>
2020-01-26 17:15:32 -0600

    Allow other loaders to register dynamic_yaml tags

--- dynamic_yaml/__init__.py
+++ dynamic_yaml/__init__.py
@@ -4,41 +4,40 @@ from .yaml_wrappers import YamlDict, YamlList, YamlEval, YamlBlockEval, YamlImpo
 from ._version import __version__  # noqa
 
 
+def register_dynamic_yaml(Loader):
+
+    def _construct_sequence(loader, node):
+        return YamlList(loader.construct_object(child) for child in node.value)
+
+    def _construct_mapping(loader, node):
+        make_obj = loader.construct_object
+        return YamlDict((make_obj(k), make_obj(v)) for k, v in node.value)
+
+    def _construct_eval(loader, node):
+        return YamlEval(node.value)
+
+    def _construct_blockeval(loader, node):
+        return YamlBlockEval(node.value)
+
+    def _construct_importer(laoder, node):
+        return YamlImport(node.value)
+
+    def _construct_includer(loader, node):
+        return YamlInclude(node.value)
+
+    Loader.add_constructor(yaml.resolver.BaseResolver.DEFAULT_SEQUENCE_TAG, _construct_sequence)
+    Loader.add_constructor(yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, _construct_mapping)
+    Loader.add_constructor('!Eval', _construct_eval)
+    Loader.add_constructor('!BlockEval', _construct_blockeval)
+    Loader.add_constructor('!Import', _construct_importer)
+    Loader.add_constructor('!Include', _construct_includer)
+
+
 class DynamicYamlLoader(yaml.FullLoader):
     pass
 
 
-def _construct_sequence(loader, node):
-    return YamlList(loader.construct_object(child) for child in node.value)
-
-
-def _construct_mapping(loader, node):
-    make_obj = loader.construct_object
-    return YamlDict((make_obj(k), make_obj(v)) for k, v in node.value)
-
-
-def _construct_eval(loader, node):
-    return YamlEval(node.value)
-
-
-def _construct_blockeval(loader, node):
-    return YamlBlockEval(node.value)
-
-
-def _construct_importer(laoder, node):
-    return YamlImport(node.value)
-
-
-def _construct_includer(loader, node):
-    return YamlInclude(node.value)
-
-
-DynamicYamlLoader.add_constructor(yaml.resolver.BaseResolver.DEFAULT_SEQUENCE_TAG, _construct_sequence)
-DynamicYamlLoader.add_constructor(yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, _construct_mapping)
-DynamicYamlLoader.add_constructor('!Eval', _construct_eval)
-DynamicYamlLoader.add_constructor('!BlockEval', _construct_blockeval)
-DynamicYamlLoader.add_constructor('!Import', _construct_importer)
-DynamicYamlLoader.add_constructor('!Include', _construct_includer)
+register_dynamic_yaml(DynamicYamlLoader)
 
 
 def post_process(data):
